87efc8620b6fe0954c1096c4731110361a0dab1a
Your Name <you@example.com>
2026-04-29 15:15:50 +0800

    Base: Lab 3-3 basic experiment completed

--- kernel/vmm.c
+++ kernel/vmm.c
@@ -232,6 +232,7 @@ void user_vm_map(pagetable_t page_dir, uint64 va, uint64 size, uint64 pa, int pe
 }
 
 //
+//
 // unmap virtual address [va, va+size] (for user application).
 //
 void user_vm_unmap(pagetable_t page_dir, uint64 va, uint64 size, int free_pa)
@@ -239,5 +240,39 @@ void user_vm_unmap(pagetable_t page_dir, uint64 va, uint64 size, int free_pa)
   if (unmap_pages(page_dir, va, size, free_pa) != 0)
   {
     panic("fail to user_vm_unmap .\n");
+  }
+}
+
+//
+// debug function, print the vm space of a process. added @lab3_1
+//
+void print_proc_vmspace(process *proc)
+{
+  sprint("======\tbelow is the vm space of process%d\t========\n", proc->pid);
+  for (int i = 0; i < proc->total_mapped_region; i++)
+  {
+    sprint("-va:%lx, npage:%d, ", proc->mapped_info[i].va, proc->mapped_info[i].npages);
+    switch (proc->mapped_info[i].seg_type)
+    {
+    case CODE_SEGMENT:
+      sprint("type: CODE SEGMENT");
+      break;
+    case DATA_SEGMENT:
+      sprint("type: DATA SEGMENT");
+      break;
+    case STACK_SEGMENT:
+      sprint("type: STACK SEGMENT");
+      break;
+    case CONTEXT_SEGMENT:
+      sprint("type: TRAPFRAME SEGMENT");
+      break;
+    case SYSTEM_SEGMENT:
+      sprint("type: USER KERNEL STACK SEGMENT");
+      break;
+    case HEAP_SEGMENT:
+      sprint("type: HEAP SEGMENT");
+      break;
+    }
+    sprint(", mapped to pa:%lx\n", lookup_pa(proc->pagetable, proc->mapped_info[i].va));
   }
 }
